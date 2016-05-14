3ee454473f249082b898f96dcfe24b97a0fd0307
Ruben Pieters <ruben.pieters2@pandora.be>
2016-05-14 16:33:59 +0200

    scalajs tutorial code

--- build.sbt
+++ build.sbt
@@ -4,6 +4,8 @@ lazy val commonSettings = Seq(
   scalaVersion := "2.11.8"
 )
 
+name := "gre"
+
 lazy val root = (project in file(".")).
 // ScalaJS
 // https://www.scala-js.org/tutorial/basic/
@@ -12,3 +14,12 @@ enablePlugins(ScalaJSPlugin)
 // Disabling Rhino must be done once per sbt session. Alternatively, you can include the setting directly in your build.sbt,
 // or, in order not to disturb your teammates, in a separate .sbt file (say, local.sbt):
 scalaJSUseRhino in Global := false
+
+
+libraryDependencies ++= Seq(
+  "org.scala-js" %%% "scalajs-dom" % "0.9.0"
+
+)
+
+// force scala version
+ivyScala := ivyScala.value map { _.copy(overrideScalaVersion = true) }
--- src/main/scala/be/rubenpieters/gre/main/MainApp.scala
+++ src/main/scala/be/rubenpieters/gre/main/MainApp.scala
@@ -1,12 +1,28 @@
 package be.rubenpieters.gre.main
 
+import org.scalajs.dom
+import org.scalajs.dom.document
+
 import scala.scalajs.js.JSApp
+import scala.scalajs.js.annotation.JSExport
 
 /**
   * Created by rpieters on 14/05/2016.
   */
 object MainApp extends JSApp {
   def main(): Unit = {
-    println("Running GRE MainApp...")
+    appendPar(document.body, "Running GRE MainApp...")
+  }
+
+  @JSExport
+  def addClickedMessage(): Unit = {
+    appendPar(document.body, "You clicked the button!")
+  }
+
+  def appendPar(targetNode: dom.Node, text: String): Unit = {
+    val parNode = document.createElement("p")
+    val textNode = document.createTextNode(text)
+    parNode.appendChild(textNode)
+    targetNode.appendChild(parNode)
   }
 }
