8fa7d2045e29c12cf669b89095162cede9b8629a
ruben <rubnepieters@gmail.com>
2016-07-12 20:33:57 +0200

    comment out scalajs code to compile scala on other pc

--- src/main/scala/be/rubenpieters/gre/main/MainApp.scala
+++ src/main/scala/be/rubenpieters/gre/main/MainApp.scala
@@ -1,39 +1,39 @@
-package be.rubenpieters.gre.main
-
-import be.rubenpieters.gre.engine.examples.simplegame.SimpleGameEngine
-import be.rubenpieters.gre.log.LogListener
-import org.scalajs.dom
-import org.scalajs.dom.document
-
-import scala.scalajs.js.JSApp
-import scala.scalajs.js.annotation.JSExport
-
-/**
-  * Created by rpieters on 14/05/2016.
-  */
-object MainApp extends JSApp {
-  val engineRunner = SimpleGameEngine.simpleEngineWithLoggers(Set(AppendParLogLister))
-
-  def main(): Unit = {
-    appendPar(document.body, "GRE")
-  }
-
-  @JSExport
-  def addClickedMessage(): Unit = {
-    engineRunner.runStep()
-  }
-
-  def appendPar(targetNode: dom.Node, text: String): Unit = {
-    val parNode = document.createElement("p")
-    val textNode = document.createTextNode(text)
-    parNode.appendChild(textNode)
-    targetNode.appendChild(parNode)
-  }
-}
-
-object AppendParLogLister extends LogListener {
-  override def log(logLine: String): Unit = {
-    MainApp.appendPar(document.body, logLine)
-  }
-}
-
+//package be.rubenpieters.gre.main
+//
+//import be.rubenpieters.gre.engine.examples.simplegame.SimpleGameEngine
+//import be.rubenpieters.gre.log.LogListener
+//import org.scalajs.dom
+//import org.scalajs.dom.document
+//
+//import scala.scalajs.js.JSApp
+//import scala.scalajs.js.annotation.JSExport
+//
+///**
+//  * Created by rpieters on 14/05/2016.
+//  */
+//object MainApp extends JSApp {
+//  val engineRunner = SimpleGameEngine.simpleEngineWithLoggers(Set(AppendParLogLister))
+//
+//  def main(): Unit = {
+//    appendPar(document.body, "GRE")
+//  }
+//
+//  @JSExport
+//  def addClickedMessage(): Unit = {
+//    engineRunner.runStep()
+//  }
+//
+//  def appendPar(targetNode: dom.Node, text: String): Unit = {
+//    val parNode = document.createElement("p")
+//    val textNode = document.createTextNode(text)
+//    parNode.appendChild(textNode)
+//    targetNode.appendChild(parNode)
+//  }
+//}
+//
+//object AppendParLogLister extends LogListener {
+//  override def log(logLine: String): Unit = {
+//    MainApp.appendPar(document.body, logLine)
+//  }
+//}
+//
