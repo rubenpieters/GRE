c95b4e67a0a6db3f35bc0c121590cc17a1a0c435
ruben <rubnepieters@gmail.com>
2016-09-01 19:49:40 +0200

    CyclicRuleStrategy

--- build.sbt
+++ build.sbt
@@ -4,8 +4,11 @@ scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
   "org.scalaz" %% "scalaz-core" % "7.3.0-M4"
+  ,"com.google.guava" % "guava" % "19.0"
+
   ,"com.typesafe.scala-logging" %% "scala-logging" % "3.4.0"
   ,"ch.qos.logback" %  "logback-classic" % "1.1.7"
+
 
   ,"org.scalatest" %% "scalatest" % "3.0.0" % "test"
   ,"org.mockito" % "mockito-all" % "1.9.5" % "test"
---
+++ src/main/scala/be/rubenpieters/gre/RuleAdvanceStrategy.scala
@@ -0,0 +1,22 @@
+package be.rubenpieters.gre
+
+/**
+  * Created by ruben on 1/09/2016.
+  */
+trait RuleAdvanceStrategy {
+  def nextStrategy: RuleAdvanceStrategy
+  def rule: AbstractRule
+}
+
+case class CyclicRuleStrategy(ruleSeq: Seq[AbstractRule], pointer: Int) extends RuleAdvanceStrategy {
+  require(pointer > 0)
+  require(pointer < ruleSeq.size)
+
+  override val rule: AbstractRule = ruleSeq(pointer)
+
+  lazy val nextPointer = pointer + 1
+  override lazy val nextStrategy: RuleAdvanceStrategy = nextPointer > ruleSeq.size match {
+    case true => CyclicRuleStrategy(ruleSeq, 0)
+    case false => CyclicRuleStrategy(ruleSeq, nextPointer)
+  }
+}
