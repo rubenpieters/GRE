c2222d4202e6de7d215a21ccf2f5ecca0c6bc623
rubenpieters <ruben.pieters@devoteam.com>
2016-10-30 21:52:17 +0100

    ser/deser functions and store them

--- src/it/scala/it/be/rubenpieters/model/StrategyPersistIT.scala
+++ src/it/scala/it/be/rubenpieters/model/StrategyPersistIT.scala
@@ -20,14 +20,14 @@ class StrategyPersistIT extends FlatSpec with Matchers with BeforeAndAfterAll wi
     // the wait strategy doesnt seem to work correctly, so ill just do it with this for now
     Thread.sleep(6000)
 
-    val createTable: Update0 = sql"CREATE TABLE IF NOT EXISTS strategy (id integer NOT NULL, name varchar NOT NULL)".update
+    val createTable: Update0 = sql"CREATE TABLE IF NOT EXISTS strategy (id integer NOT NULL, name varchar NOT NULL, func bytea NOT NULL)".update
 
-    def insert1(id: Int, name: String): Update0 =
-      sql"insert into strategy (id, name) values ($id, $name)".update
+    def insert1(id: Int, name: String, func: Int => Int): Update0 =
+      sql"insert into strategy (id, name, func) values ($id, $name, ${Serialization.serialise(func)})".update
 
-    val insert: Update0 = insert1(1, "strat_1")
+    val insert: Update0 = insert1(1, "strat_1", x => x + 1)
 
-    val select: Query0[(Int, String)] = sql"select id, name from strategy".query[(Int, String)]
+    val select: Query0[(Int, String, Array[Byte])] = sql"select id, name, func from strategy".query[(Int, String, Array[Byte])]
 
     val xa = DriverManagerTransactor[IOLite]("org.postgresql.Driver", postgresJdbcConnStr, PostgresContainer.postgresUser, PostgresContainer.postgresPw)
 
@@ -38,6 +38,32 @@ class StrategyPersistIT extends FlatSpec with Matchers with BeforeAndAfterAll wi
     println(result.unsafePerformIO)
 
     val resultSel = select.list.transact(xa)
-    println(resultSel.unsafePerformIO)
+    val resultTuple = resultSel.unsafePerformIO
+    println(resultTuple)
+    val func = Serialization.deserialise(resultTuple.head._3).asInstanceOf[Int => Int]
+    println(func(5))
+  }
+}
+
+
+// use this for now, should also investigate kryo serialization
+// http://stackoverflow.com/a/39371571/5559685
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, ObjectInputStream, ObjectOutputStream}
+
+object Serialization extends App {
+
+  def serialise(value: Any): Array[Byte] = {
+    val stream: ByteArrayOutputStream = new ByteArrayOutputStream()
+    val oos = new ObjectOutputStream(stream)
+    oos.writeObject(value)
+    oos.close
+    stream.toByteArray
+  }
+
+  def deserialise(bytes: Array[Byte]): Any = {
+    val ois = new ObjectInputStream(new ByteArrayInputStream(bytes))
+    val value = ois.readObject
+    ois.close
+    value
   }
 }
