e451c9f7f899393fb791de709c3d5b674fd8e32c
rubenpieters <ruben.pieters@devoteam.com>
2016-10-31 10:24:06 +0100

    start rpsenginerunner

---
+++ src/main/scala/be/rubenpieters/model/rps/RPSEngineRunner.scala
@@ -0,0 +1,20 @@
+package be.rubenpieters.model.rps
+
+import be.rubenpieters.model.Strategy
+
+/**
+  * Created by ruben on 31/10/16.
+  */
+object RPSEngineRunner {
+  // TODO: generalize these functions
+  // TODO: change/generalize output type
+  def turnWinner(strategy1: Strategy[RpsInput, RpsOutput], strategy2: Strategy[RpsInput, RpsOutput]): Int = {
+    val action1 = strategy1.getAction(())
+    val action2 = strategy2.getAction(())
+    actionCompare(action1, action2)
+  }
+
+  def actionCompare(action1: RpsOutput, action2: RpsOutput): Int = {
+    1
+  }
+}
