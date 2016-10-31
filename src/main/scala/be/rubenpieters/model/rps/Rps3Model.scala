f648206693443306c8b51019555cb6d2dec0f669
ruben <rubnepieters@gmail.com>
2016-10-31 15:07:21 +0100

    remove unneeded brackets in rules decls

--- src/main/scala/be/rubenpieters/model/rps/Rps3Model.scala
+++ src/main/scala/be/rubenpieters/model/rps/Rps3Model.scala
@@ -9,36 +9,30 @@ import be.rubenpieters.util.Inverse.ops._
 sealed trait Rps3Model
 case object Rock extends Rps3Model {
   implicit object RockRules extends RpsRules[this.type] {
-    override def beats[B: RpsRules](a: Rock.this.type, b: B): RpsWinState = {
-      b match {
-        case Rock => Tie
-        case _ => b.beats(a).inverse
-      }
+    override def beats[B: RpsRules](a: Rock.this.type, b: B): RpsWinState = b match {
+      case Rock => Tie
+      case _ => b.beats(a).inverse
     }
   }
 }
 
 case object Paper extends Rps3Model {
   implicit object PaperRules extends RpsRules[this.type] {
-    override def beats[B: RpsRules](a: Paper.this.type, b: B): RpsWinState = {
-      b match {
-        case Rock => Win
-        case Paper => Tie
-        case _ => b.beats(a).inverse
-      }
+    override def beats[B: RpsRules](a: Paper.this.type, b: B): RpsWinState = b match {
+      case Rock => Win
+      case Paper => Tie
+      case _ => b.beats(a).inverse
     }
   }
 }
 
 case object Scissors extends Rps3Model {
   implicit object ScissorsRules extends RpsRules[this.type] {
-    override def beats[B: RpsRules](a: Scissors.this.type, b: B): RpsWinState = {
-      b match {
-        case Rock => Loss
-        case Paper => Win
-        case Scissors => Tie
-        case _ => b.beats(a).inverse
-      }
+    override def beats[B: RpsRules](a: Scissors.this.type, b: B): RpsWinState = b match {
+      case Rock => Loss
+      case Paper => Win
+      case Scissors => Tie
+      case _ => b.beats(a).inverse
     }
   }
 }
