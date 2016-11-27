19c14aad71542f01c67c59cfbdd3e9e7b0555e80
ruben <rubnepieters@gmail.com>
2016-11-27 13:36:20 +0100

    different model, first compile

--- src/main/scala/be/rubenpieters/model/SubGame.scala
+++ src/main/scala/be/rubenpieters/model/SubGame.scala
@@ -3,11 +3,12 @@ package be.rubenpieters.model
 import cats.data.State
 
 /**
+  * input stays the same in the subgame chain
+  * state is carried on from the previous to the next subgame in the subgame chain
+  *
   * Created by ruben on 26/11/2016.
   */
-case class SubGame[I, S, O](name: String, input: I, valueState: S, operation: I => State[S, O], runningState: GameRunningState) {
-
-}
+case class SubGame[I, S, O](name: String, input: I, operation: I => State[S, Either[O, SubGame[I, S, O]]])
 
 object SubGame {
 //  def run(runner: )
--- src/main/scala/be/rubenpieters/model/number/NumberRunner.scala
+++ src/main/scala/be/rubenpieters/model/number/NumberRunner.scala
@@ -2,7 +2,7 @@ package be.rubenpieters.model.number
 
 import cats._
 import cats.implicits._
-import be.rubenpieters.model.{Idle, SubGame}
+import be.rubenpieters.model._
 import be.rubenpieters.util.ImmutableRng
 import cats.data.State
 
@@ -11,24 +11,36 @@ import cats.data.State
   */
 object NumberRunner extends App {
   val initialRng = ImmutableRng.scrambled(1)
-  val numberGameRun: State[ImmutableRng, Int] = for {
+
+  type NGI = Unit
+  type NGS = ImmutableRng
+  type NGO = Int
+  val numberGameRun: NGI => State[NGS, Either[NGO, SubGame[NGI, NGS, NGO]]] = _ => for {
     toGetNumbers <- (1 to 5).toList.traverseU(_ => ImmutableRng.nextLong() map math.abs)
     assemblingNumbers <- (1 to 10).toList.traverseU(_ => ImmutableRng.nextInt(50) map math.abs)
     assemblyGame1 = createNumberAssemblyGame(1, toGetNumbers.head, assemblingNumbers)
     assemblyGame2 = createNumberAssemblyGame(2, toGetNumbers.head, assemblingNumbers)
-  } yield -1
+    game1Result <- SubGameRunner.run(assemblyGame1)
+    game2Result <- SubGameRunner.run(assemblyGame2)
+  } yield Left(-1): Either[NGO, SubGame[NGI, NGS, NGO]]
 
-  val numberGame = SubGame[Unit, ImmutableRng, Int]("number_game", (), initialRng, _ => numberGameRun, Idle)
+  val numberGame = SubGame[NGI, NGS, NGO]("number_game", (), numberGameRun)
 
 
-  val assemblyGameRun: (Long, List[Int]) => State[Unit, (List[Int], List[Int] => Long)] =
-    (toGetNumber, assemblingNumbers) =>
-      State.pure{
-        (List[Int](), (a: List[Int]) => 0L)
+  type AGI = (Long, List[Int])
+  type AGS = ImmutableRng
+  type AGO = (List[Int], List[Int] => Long)
+  val assemblyGameRun: AGI => State[AGS, Either[AGO, SubGame[AGI, AGS, AGO]]] = {
+    case ((toGetNumber, assemblingNumbers)) =>
+      State.pure {
+        Left((List[Int](), (a: List[Int]) => 0L))
       }
+  }
   def createNumberAssemblyGame(id: Int, toGetNumber: Long, assemblingNumbers: List[Int]) =
-    SubGame[(Long, List[Int]), Unit, (List[Int], List[Int] => Long)](
-      s"assembly_game_$id", (toGetNumber, assemblingNumbers), (), assemblyGameRun.tupled, Idle
+    SubGame[AGI, AGS, AGO](
+      s"assembly_game_$id", (toGetNumber, assemblingNumbers), assemblyGameRun
     )
 
+
+  println(SubGameRunner.run(numberGame).run(initialRng).value)
 }
