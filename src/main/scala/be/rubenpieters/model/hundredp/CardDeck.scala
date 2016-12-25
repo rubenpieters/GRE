90b50cd47495c769d797c9f8edc97faf49f17adb
rubenpieters <ruben.pieters@devoteam.com>
2016-12-25 22:12:56 +0100

    draw with reshuffle

--- src/main/scala/be/rubenpieters/model/hundredp/CardDeck.scala
+++ src/main/scala/be/rubenpieters/model/hundredp/CardDeck.scala
@@ -1,5 +1,8 @@
 package be.rubenpieters.model.hundredp
 
+import be.rubenpieters.util.ImmutableRng
+import cats.data.State
+
 /**
   * Created by ruben on 18/12/16.
   */
@@ -17,4 +20,18 @@ object CardDeck {
       draw(reshuffledDeck, CardDiscard(List()), x)
     }
   }
+
+  def drawWithShuffle(deck: CardDeck, discard: CardDiscard, x: Int): State[ImmutableRng, (List[Card], CardDeck, CardDiscard)] = {
+    require(x <= deck.cards.size + discard.cards.size)
+    if (deck.cards.size >= x) {
+      State.pure((deck.cards.take(x), CardDeck(deck.cards.drop(x)), discard))
+    } else {
+      for (reshuffledDeck <- shuffle(CardDeck(deck.cards ++ discard.cards)))
+        yield draw(reshuffledDeck, CardDiscard(List()), x)
+    }
+  }
+
+  def shuffle(deck: CardDeck): State[ImmutableRng, CardDeck] = for {
+    shuffled <- ImmutableRng.shuffle(deck.cards)
+  } yield CardDeck(shuffled)
 }
--- src/main/scala/be/rubenpieters/model/hundredp/HundredPGame.scala
+++ src/main/scala/be/rubenpieters/model/hundredp/HundredPGame.scala
@@ -1,5 +1,7 @@
 package be.rubenpieters.model.hundredp
 
+import be.rubenpieters.util.ImmutableRng
+
 /**
   * Created by ruben on 18/12/16.
   */
@@ -8,23 +10,43 @@ object HundredPGame extends App {
   val cardDiscard = CardDiscard(List())
   val cardDeck = CardDeck(List(AddXToField(1), AddXToField(2), AddXToField(3), AddXToField(4)))
 
-  val (field, discard, deck) = handleTurn(cardField, cardDiscard, cardDeck)
-    println(field)
-    println(discard)
-    println(deck)
-    println("+-----------")
-  val (field2, discard2, deck2) = handleTurn(field, discard, deck)
-    println(field2)
-    println(discard2)
-    println(deck2)
-    println("+-----------")
+  //  playX(cardField, cardDiscard, cardDeck, 3).foreach(println)
+  //  val (field, discard, deck) = handleTurn(cardField, cardDiscard, cardDeck)
+  //    println(field)
+  //    println(discard)
+  //    println(deck)
+  //    println("+-----------")
+  //  val (field2, discard2, deck2) = handleTurn(field, discard, deck)
+  //    println(field2)
+  //    println(discard2)
+  //    println(deck2)
+  //    println("+-----------")
+
+  val play2 = for {
+    t1 <- handleTurn(cardField, cardDiscard, cardDeck)
+    (cardField1, cardDiscard1, cardDeck1) = t1
+    _ = println(t1)
+    t2 <- handleTurn(cardField1, cardDiscard1, cardDeck1)
+    (cardField2, cardDiscard2, cardDeck2) = t2
+    _ = println(t2)
+  } yield t2
+
+  play2.run(ImmutableRng.scrambled(1)).value
 
   def handleTurn(cardField: CardContainer, cardDiscard: CardDiscard, cardDeck: CardDeck) = {
-    val (drawnCards, newDeck, newDiscard) = CardDeck.draw(cardDeck, cardDiscard, 3)
-    val hand = CardHand(drawnCards)
-    hand.cards.foldLeft((cardField: CardContainer, newDiscard, newDeck)){ case ((field, discard, deck), card) =>
-      val (newField, newDiscard) = Card.playAndDiscard(card, field, discard)
-      (newField, newDiscard, deck)
+    for {
+      s <- CardDeck.drawWithShuffle(cardDeck, cardDiscard, 3)
+      (drawnCards, newDeck, newDiscard) = s
+    } yield {
+      val hand = CardHand(drawnCards)
+      hand.cards.foldLeft((cardField: CardContainer, newDiscard, newDeck)) { case ((field, discard, deck), card) =>
+        val (newField, newDiscard) = Card.playAndDiscard(card, field, discard)
+        (newField, newDiscard, deck)
+      }
     }
   }
+
+  //  def playX(cardField: CardContainer, cardDiscard: CardDiscard, cardDeck: CardDeck, x: Int) = {
+  //    List.iterate((cardField, cardDiscard, cardDeck), x)((handleTurn _).tupled)
+  //  }
 }
